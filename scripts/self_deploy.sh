81e438de02d45fa74101a61dae9dd6293a700882
bittokazi <bitto.kazi@gmail.com>
2020-05-01 02:47:31 +0600

    fix

--- scripts/self_deploy.sh
+++ scripts/self_deploy.sh
@@ -1,14 +1,14 @@
 pm2 stop nodejs-web-app-deployer
 pm2 delete nodejs-web-app-deployer
-rm -rf web-application-deployer
-git clone https://github.com/bittokazi/web-application-deployer.git
-cd web-application-deployer
+rm -rf web-application-deployer-nodejs
+git clone https://github.com/bittokazi/web-application-deployer-nodejs.git
+cd web-application-deployer-nodejs
 npm install
 cd frontend
 npm install
 npm run build:stage
 cp -r build ../spaBuild
 cd ../
-cp .env web-application-deployer/.env
-cd web-application-deployer
+cp .env web-application-deployer-nodejs/.env
+cd web-application-deployer-nodejs
 pm2 start npm --name "nodejs-web-app-deployer" -- start
