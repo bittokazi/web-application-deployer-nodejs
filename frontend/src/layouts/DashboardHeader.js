0a8cc5468b53f42ee1bb3a606301cb817c4c0098
bittokazi <bitto.kazi@gmail.com>
2020-05-01 09:30:17 +0600

    self deploy log

--- frontend/src/layouts/DashboardHeader.js
+++ frontend/src/layouts/DashboardHeader.js
@@ -20,7 +20,7 @@ export default function DashboardHeader() {
             <div class="top-left-part">
               <a class="logo">
                 <i class="glyphicon glyphicon-fire"></i>&nbsp;
-                <span class="hidden-xs">PMBT</span>
+                <span class="hidden-xs">V1.0</span>
               </a>
             </div>
             <ul class="nav navbar-top-links navbar-left hidden-xs">
