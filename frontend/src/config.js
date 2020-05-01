e1051d4009c068ba8f37eae3af204c71b82e45d1
bittokazi <bitto.kazi@gmail.com>
2020-05-02 00:38:57 +0600

    baseurl fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,9 +5,9 @@ const dev = {
 };
 
 const stage = {
-  API_BASE_URL: "localhost:8081",
-  API_BASE_URL_PROTOCOL: "http://",
-  CHAT_SERVER_URL: "http://localhost:8081",
+  API_BASE_URL: "cloud.bitto.website",
+  API_BASE_URL_PROTOCOL: "https://",
+  CHAT_SERVER_URL: "https://cloud.bitto.website",
 };
 
 const prod = {
