35412743423600ec545ad2c312683627d30ced62
bittokazi <bitto.kazi@gmail.com>
2020-05-07 22:29:27 +0600

    deploy image from docker hub

--- src/controllers/DockerDeployController.js
+++ src/controllers/DockerDeployController.js
@@ -1,6 +1,8 @@
 import { dockerDeployApplication } from "../service/ApplicationService";
 
 export const dockerDeployController = (req, res) => {
+  res.status(200).json({ status: "ok" });
+  return;
   dockerDeployApplication(
     req,
     (result) => {
