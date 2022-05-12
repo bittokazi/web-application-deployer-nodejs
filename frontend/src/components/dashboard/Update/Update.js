a043e934bd2cd175fcf07ef9c7daefad63b1e8be
bittokazi <bitto.kazi@gmail.com>
2022-05-12 14:40:24 +0200

    add log file

--- frontend/src/components/dashboard/Update/Update.js
+++ frontend/src/components/dashboard/Update/Update.js
@@ -96,7 +96,7 @@ export default class Update extends Component {
               <div class="row">
                 <div class="col-md-12">
                   <div class="white-box">
-                    <h5>Current Version: 1.3.7.rc_5</h5>
+                    <h5>Current Version: 1.4.0.rc_5</h5>
                     <br></br>
                     <button
                       class="btn btn-info waves-effect waves-light"
